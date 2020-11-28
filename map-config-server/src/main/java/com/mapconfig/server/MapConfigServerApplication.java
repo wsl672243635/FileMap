85a9a57db420de83dc1a9d4474bd140c04ffc59b
Dio Wong <672243635@qq.com>
2020-11-28 12:20:30 +0800

    更新项目Config

---
+++ map-config-server/src/main/java/com/mapconfig/server/MapConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.mapconfig.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EnableConfigServer
+@SpringBootApplication
+public class MapConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MapConfigServerApplication.class,args);
+    }
+}
